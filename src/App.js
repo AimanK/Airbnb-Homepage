4c42f7ffcfd614b799a72dbb98b63e74235f036f
Aiman Kayad <Akaya005@fiu.edu>
2021-10-12 10:54:39 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import Home from './Home';
+import Header from './Header';
+import Footer from './Footer';
+
+function App() {
+  return (
+
+     //BEM
+
+    <div className="App">
+      <Header />
+
+      <Home />
+
+      <Footer />
+  
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+    return (
+        <div className = "footer">
+            <p>Developed by Aiman Kayad. No rights reserved, this is only for learning purposes!</p>
+        </div>
+    )
+}
+
+export default Footer
